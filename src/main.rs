4750600d1373339af2341df95446c0b17e7fc633
Tyler Lutz <hello@tylerlutz.dev>
2024-03-19 17:09:55 -0400

    feat: progress indicator

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     println!("P3\n{} {}\n255", image_width, image_height);
 
     for j in 0..image_height {
+        eprint!("\rScanlines remaining: {:3}", (image_height - j));
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
@@ -17,4 +18,6 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+
+    eprintln!("\rDone!                     ")
 }
