f806c35480a7636a27bf434623b93f097a953ffe
Tyler Lutz <hello@tylerlutz.dev>
2024-03-21 11:59:03 -0400

    feat: add a sphere

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,20 @@ mod vector3;
 use ray::Ray;
 use vector3::Vector3;
 
+fn hit_sphere(center: Vector3, radius: f64, ray: &Ray) -> bool {
+    let oc = ray.origin - center;
+    let a = ray.direction.dot(ray.direction);
+    let b = 2.0 * oc.dot(ray.direction);
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
+}
+
 fn ray_color(ray: &Ray) -> Vector3 {
+    if hit_sphere(Vector3::new(0.0, 0.0, -1.0), 0.5, ray) {
+        return Vector3::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = ray.direction.normalized();
     let a = 0.5 * (unit_direction.y + 1.0);
     (1.0 - a) * Vector3::new(1.0, 1.0, 1.0) + a * Vector3::new(0.5, 0.7, 1.0)
@@ -29,7 +42,7 @@ fn main() {
 
     let focal_length = 1.0;
     let viewport_height = 2.0;
-    let viewport_width = viewport_height * (image_width / image_height) as f64;
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
     let camera_center = Vector3::new(0.0, 0.0, 0.0);
 
     let viewport_u = Vector3::new(viewport_width, 0.0, 0.0);
