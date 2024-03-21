b752fc17cf269d36d04bd0ea4b53f4d6160d10f0
Tyler Lutz <hello@tylerlutz.dev>
2024-03-21 12:12:39 -0400

    feat: surface normal shading

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,25 @@ mod vector3;
 use ray::Ray;
 use vector3::Vector3;
 
-fn hit_sphere(center: Vector3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: Vector3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.origin - center;
     let a = ray.direction.dot(ray.direction);
     let b = 2.0 * oc.dot(ray.direction);
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant >= 0.0
+
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
 fn ray_color(ray: &Ray) -> Vector3 {
-    if hit_sphere(Vector3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        return Vector3::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Vector3::new(0.0, 0.0, -1.0), 0.5, ray);
+    if t > 0.0 {
+        let n = (ray.at(t) - Vector3::new(0.0, 0.0, -1.0)).normalized();
+        return 0.5 * Vector3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
     }
 
     let unit_direction = ray.direction.normalized();
