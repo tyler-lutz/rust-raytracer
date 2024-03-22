7c00ca4537e03552b1f7710d33f887916b36e521
Tyler Lutz <hello@tylerlutz.dev>
2024-03-22 11:59:23 -0400

    refactor: new struct for camer and render loop

---
+++ src/camera.rs
@@ -0,0 +1,105 @@
+use crate::{
+    hittable::{HitRecord, Hittable},
+    interval::Interval,
+    ray::Ray,
+    vector3::Vector3,
+};
+
+pub struct Camera {
+    pub aspect_ratio: f64,
+    pub image_width: i32,
+    image_height: i32,
+    center: Vector3,
+    pixel00_loc: Vector3,
+    pixel_delta_u: Vector3,
+    pixel_delta_v: Vector3,
+}
+
+impl Camera {
+    fn initialize(&mut self) {
+        self.image_height = (self.image_width as f64 / self.aspect_ratio) as i32;
+        if self.image_height < 1 {
+            self.image_height = 1;
+        }
+
+        self.center = Vector3::new(0.0, 0.0, 0.0);
+
+        // Determine viewport dimensions
+        let focal_length = 1.0;
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * (self.image_width as f64 / self.image_height as f64);
+
+        // Calculate the vectors across the horizontal and down the vertical viewport edges
+        let viewport_u = Vector3::new(viewport_width, 0.0, 0.0);
+        let viewport_v = Vector3::new(0.0, -viewport_height, 0.0);
+
+        self.pixel_delta_u = viewport_u / self.image_width as f64;
+        self.pixel_delta_v = viewport_v / self.image_height as f64;
+
+        let viewport_upper_left = self.center
+            - Vector3::new(0.0, 0.0, focal_length)
+            - viewport_u / 2.0
+            - viewport_v / 2.0;
+        self.pixel00_loc = viewport_upper_left + 0.5 * (self.pixel_delta_u + self.pixel_delta_v);
+    }
+
+    pub fn render(&mut self, world: &dyn Hittable) {
+        self.initialize();
+
+        println!("P3\n{} {}\n255", self.image_width, self.image_height);
+        for j in 0..self.image_height {
+            eprint!("\rScanlines remaining: {:3}", (self.image_height - j));
+            for i in 0..self.image_width {
+                let pixel_center = self.pixel00_loc
+                    + (i as f64 * self.pixel_delta_u)
+                    + (j as f64 * self.pixel_delta_v);
+                let ray_direction = pixel_center - self.center;
+                let ray = Ray::new(self.center, ray_direction);
+
+                let pixel_color = self.ray_color(&ray, world);
+
+                self.write_color(&pixel_color);
+            }
+        }
+
+        eprintln!("\rDone!                     ")
+    }
+
+    fn write_color(&self, pixel_color: &Vector3) {
+        println!(
+            "{} {} {}",
+            (255.999 * pixel_color.x) as i32,
+            (255.999 * pixel_color.y) as i32,
+            (255.999 * pixel_color.z) as i32
+        )
+    }
+
+    fn ray_color(&self, ray: &Ray, world: &dyn Hittable) -> Vector3 {
+        let mut hit_record = HitRecord {
+            point: Vector3::new(0.0, 0.0, 0.0),
+            normal: Vector3::new(0.0, 0.0, 0.0),
+            t: 0.0,
+        };
+        if world.hit(ray, Interval::new(0.0, f64::INFINITY), &mut hit_record) {
+            return 0.5 * (hit_record.normal + Vector3::new(1.0, 1.0, 1.0));
+        }
+
+        let unit_direction = ray.direction.normalized();
+        let a = 0.5 * (unit_direction.y + 1.0);
+        (1.0 - a) * Vector3::new(1.0, 1.0, 1.0) + a * Vector3::new(0.5, 0.7, 1.0)
+    }
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        Camera {
+            aspect_ratio: 1.0,
+            image_width: 100,
+            image_height: 0,
+            center: Vector3::new(0.0, 0.0, 0.0),
+            pixel00_loc: Vector3::new(0.0, 0.0, 0.0),
+            pixel_delta_u: Vector3::new(0.0, 0.0, 0.0),
+            pixel_delta_v: Vector3::new(0.0, 0.0, 0.0),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod camera;
 mod hittable;
 mod hittable_list;
 mod interval;
@@ -5,45 +6,11 @@ mod ray;
 mod sphere;
 mod vector3;
 
-use hittable::{HitRecord, Hittable};
-use interval::Interval;
-use ray::Ray;
+use camera::Camera;
 use vector3::Vector3;
 
 use crate::{hittable_list::HittableList, sphere::Sphere};
-
-fn ray_color(ray: &Ray, world: &dyn Hittable) -> Vector3 {
-    let mut hit_record = HitRecord {
-        point: Vector3::new(0.0, 0.0, 0.0),
-        normal: Vector3::new(0.0, 0.0, 0.0),
-        t: 0.0,
-    };
-    if world.hit(ray, Interval::new(0.0, f64::INFINITY), &mut hit_record) {
-        return 0.5 * (hit_record.normal + Vector3::new(1.0, 1.0, 1.0));
-    }
-
-    let unit_direction = ray.direction.normalized();
-    let a = 0.5 * (unit_direction.y + 1.0);
-    (1.0 - a) * Vector3::new(1.0, 1.0, 1.0) + a * Vector3::new(0.5, 0.7, 1.0)
-}
-
-fn write_color(pixel_color: &Vector3) {
-    println!(
-        "{} {} {}",
-        (255.999 * pixel_color.x) as i32,
-        (255.999 * pixel_color.y) as i32,
-        (255.999 * pixel_color.z) as i32
-    )
-}
-
 fn main() {
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
-    let mut image_height = (image_width as f64 / aspect_ratio) as i32;
-    if image_height < 1 {
-        image_height = 1;
-    }
-
     let mut world = HittableList::new();
 
     world.add(Box::new(Sphere::new(Vector3::new(0.0, 0.0, -1.0), 0.5)));
@@ -52,36 +19,10 @@ fn main() {
         100.0,
     )));
 
-    let focal_length = 1.0;
-    let viewport_height = 2.0;
-    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
-    let camera_center = Vector3::new(0.0, 0.0, 0.0);
+    let mut camera = Camera::default();
 
-    let viewport_u = Vector3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vector3::new(0.0, -viewport_height, 0.0);
+    camera.aspect_ratio = 16.0 / 9.0;
+    camera.image_width = 400;
 
-    let pixel_delta_u = viewport_u / image_width as f64;
-    let pixel_delta_v = viewport_v / image_height as f64;
-
-    let viewport_upper_left =
-        camera_center - Vector3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
-    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    println!("P3\n{} {}\n255", image_width, image_height);
-
-    for j in 0..image_height {
-        eprint!("\rScanlines remaining: {:3}", (image_height - j));
-        for i in 0..image_width {
-            let pixel_center =
-                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
-            let ray_direction = pixel_center - camera_center;
-            let ray = Ray::new(camera_center, ray_direction);
-
-            let pixel_color = ray_color(&ray, &world);
-
-            write_color(&pixel_color);
-        }
-    }
-
-    eprintln!("\rDone!                     ")
+    camera.render(&world);
 }
